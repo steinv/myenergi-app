fad226b32ce36555fa884acad00e6af711f9e785
stein <stein.vanmassenhove@kbc.be>
2021-09-01 14:47:05 +0200

    map env props to spring boot

--- src/main/java/com/stein/myenergi/DatabaseConfig.java
+++ src/main/java/com/stein/myenergi/DatabaseConfig.java
@@ -8,15 +8,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+/**
+ * Provide necessary environment variables
+ * https://data.heroku.com/datastores/
+ */
 public class DatabaseConfig {
 
     @Value("${spring.datasource.url}")
     private String dbUrl;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
+        config.setUsername(username);
+        config.setPassword(password);
         return new HikariDataSource(config);
     }
 }

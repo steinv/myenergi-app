296efaa41651e1941dbddf53a526f801bd8ee84a
steinv <stein.vanmassenhove@gmail.com>
2021-09-24 15:53:45 +0200

    purge tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
 module.exports = {
     prefix: '',
+    mode: 'jit',
     purge: {
       content: [
-        './src/**/*.{html,ts,js}',
+        './src/**/*.{html,ts,css,scss,sass,less,styl}',
       ]
     },
     darkMode: 'class', // or 'media' or 'class'

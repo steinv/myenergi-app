80d48a444eb9d4f973e5925a302d8b8808d8008c
steinv <stein.vanmassenhove@gmail.com>
2021-09-25 10:24:38 +0200

    ignore unknown fields

--- backend/src/main/java/com/stein/myenergi/dto/PersistInput.java
+++ backend/src/main/java/com/stein/myenergi/dto/PersistInput.java
@@ -1,12 +1,13 @@
 package com.stein.myenergi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.Calendar;
 import java.util.List;
 
 @Data
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PersistInput {
     private List<Calendar> dates;
 }

fa04a7be68d1c0701547e198ed850078e4843c83
stein <stein.vanmassenhove@gmail.com>
2022-08-28 10:10:07 +0200

    date serialization hint

--- backend/src/main/java/com/stein/myenergi/MyEnergiApplication.java
+++ backend/src/main/java/com/stein/myenergi/MyEnergiApplication.java
@@ -3,32 +3,35 @@ package com.stein.myenergi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.ProxyBits;
+import org.springframework.nativex.hint.SerializationHint;
+import org.springframework.nativex.hint.TypeHint;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.interceptor.Retryable;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.fasterxml.jackson.databind.ser.std.SqlDateSerializer;
+import com.stein.myenergi.api.calls.DayCall;
+import com.stein.myenergi.api.calls.StatusCall;
 import com.stein.myenergi.api.dto.HistoryDay;
 import com.stein.myenergi.database.entities.HistoryEntity;
 import com.stein.myenergi.database.entities.HistoryId;
 
-import org.springframework.nativex.hint.ProxyBits;
-import org.springframework.nativex.hint.SerializationHint;
-import org.springframework.nativex.hint.TypeHint;
-
 // AOT proxy for retryable
 @AotProxyHint(
-	targetClass=com.stein.myenergi.api.calls.DayCall.class, 
-	interfaces={org.springframework.retry.interceptor.Retryable.class}, 
+	targetClass=DayCall.class, 
+	interfaces={Retryable.class}, 
 	proxyFeatures = ProxyBits.IS_STATIC
 )
 @AotProxyHint(
-	targetClass=com.stein.myenergi.api.calls.StatusCall.class, 
-	interfaces={org.springframework.retry.interceptor.Retryable.class}, 
+	targetClass=StatusCall.class, 
+	interfaces={Retryable.class}, 
 	proxyFeatures = ProxyBits.IS_STATIC
 )
 // The combined-key HistoryId and it's related classes need to be serializable for hibernate. 
 @SerializationHint(types = {HistoryId.class, HistoryDay.class, HistoryEntity.class, String.class})
 // ClassNotFoundException at runtime when this isn't hinted for the native image
-@TypeHint(types = {java.sql.Date.class})
+@TypeHint(types = {SqlDateSerializer.class})
 @SpringBootApplication
 @EnableRetry
 @EnableScheduling

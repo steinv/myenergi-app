8324ff7b755604f263f4c34ec35d3543c0f2a199
stein <stein.vanmassenhove@gmail.com>
2022-11-19 08:11:46 +0100

    update to angular 15

--- frontend/src/app/custom-date-adapter.ts
+++ frontend/src/app/custom-date-adapter.ts
@@ -1,9 +1,13 @@
-import { Injectable } from "@angular/core";
-import { NativeDateAdapter } from "@angular/material/core";
+import { Inject, Injectable } from "@angular/core";
+import { MAT_DATE_LOCALE, NativeDateAdapter } from "@angular/material/core";
 
 /** Adapts the native JS Date for use with cdk-based components that work with dates. */
 @Injectable()
 export class CustomDateAdapter extends NativeDateAdapter {
+    
+    constructor(@Inject(MAT_DATE_LOCALE) matDateLocale: string) {
+        super(matDateLocale);
+    }
     
     getFirstDayOfWeek(): number {
      return 1;
